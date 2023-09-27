42846e05dd41634681e644c5bd147d469fad4b42
Ugochi <amakomitchell5@gmail.com>
2023-09-27 09:01:50 +0200

    a complete todo app

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Text, SafeAreaView, TextInput, Alert, TouchableOpacity, FlatList } from 'react-native';
+// import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
+const App = () => {
+  const [todo, setTodo] = React.useState('');
+  const [todos, setTodos] = React.useState([]);
+
+  const handleAddTodo = () => {
+    if (todo.length > 0) {
+      setTodos([...todos, todo]);
+      setTodo('');
+    } else {
+      Alert.alert('Error', 'Please enter a todo');
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <View style={{ padding: 16 }}>
+        <View style={styles.container}>
+          <Text style={styles.header}>A Simple Todo</Text>
+          <TextInput
+            value={todo}
+            onChangeText={(text) => setTodo(text)}
+            style={styles.textBox}
+            placeholder="Add your Todo here"
+          />
+          <TouchableOpacity style={styles.addButton} onPress={handleAddTodo}>
+            <Text style={styles.buttonText}>Add Todo</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.todoList}>
+          <FlatList 
+            data={todos} 
+            renderItem={({item}) => <Text>{item}</Text>} 
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  container: { 
+    marginTop: 100
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  textBox: {
+    height: 40,
+    borderColor: 'lightblue',
+    borderWidth: 1,
+    marginBottom: 16,
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  todoList: {
+    marginTop: 16,
+  }
 });
+
+export default App;
